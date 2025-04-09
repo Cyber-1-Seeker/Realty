dc63a3c604d9416573baca967df6be21c25ed5f0
Senshi <senshi.@gmail.com>
2025-04-09 03:23:08 +0300

    animation with framer-motion

--- frontend/src/components/Home/Testimonials/Testimonials.jsx
+++ frontend/src/components/Home/Testimonials/Testimonials.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import styles from './Testimonials.module.css'
+import {motion} from "framer-motion";
 
 const Testimonials = () => {
   return (
-    <section className={styles.testimonials}>
-      <h2>Отзывы клиентов</h2>
-      <div className={styles.list}>
-        <div className={styles.item}>
-          <p>“Продали квартиру за 2 дня, всё прошло спокойно и безопасно. Очень доволен!”</p>
-          <strong>— Иван, Москва</strong>
+      <motion.section
+          className={styles.testimonials}
+          initial={{opacity: 0, y: 50}}
+          whileInView={{opacity: 1, y: 0}}
+          transition={{duration: 0.6}}
+          viewport={{once: true, amount: 0.2}}
+      >
+        <h2>Отзывы клиентов</h2>
+        <div className={styles.list}>
+          <div className={styles.item}>
+            <p>“Продали квартиру за 2 дня, всё прошло спокойно и безопасно. Очень доволен!”</p>
+            <strong>— Иван, Москва</strong>
+          </div>
+          <div className={styles.item}>
+            <p>“Получил аванс в день обращения. Полностью довольна сотрудничеством.”</p>
+            <strong>— Мария, Химки</strong>
+          </div>
+          <div className={styles.item}>
+            <p>“Не думал, что можно продать квартиру так быстро. Спасибо за помощь!”</p>
+            <strong>— Алексей, Балашиха</strong>
+          </div>
         </div>
-        <div className={styles.item}>
-          <p>“Получил аванс в день обращения. Полностью довольна сотрудничеством.”</p>
-          <strong>— Мария, Химки</strong>
-        </div>
-        <div className={styles.item}>
-          <p>“Не думал, что можно продать квартиру так быстро. Спасибо за помощь!”</p>
-          <strong>— Алексей, Балашиха</strong>
-        </div>
-      </div>
-    </section>
+      </motion.section>
   )
 }
 
