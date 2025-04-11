e708dbbe848657e585347d7df0af0e1fc2988667
Muhammad <bakanaevmohmad@gmail.com>
2025-04-12 02:59:40 +0300

    Добавление иконок и калькулятора

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Hero from './components/Hero/Hero'
 import Layout from './components/Layout/Layout'
 import About from './components/pages/About'
@@ -7,17 +7,16 @@ import "./App.css"
 import Home from "./components/Home/Home.jsx";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home/> } />
-          <Route path="/about" element={<About />} />
-          <Route path="/calculator" element={<Calculator />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout/>}>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
