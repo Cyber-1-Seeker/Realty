d5ac37d09ea0af61d70a8d047e435eaf0daa8fc1
Muhammad <bakanaevmohmad@gmail.com>
2025-04-30 23:10:06 +0300

    Настроен api для регистрации

--- backend/apartment/urls.py
+++ backend/apartment/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ApartmentListAPIView
 
 urlpatterns = [
-    path('api/apartments/', ApartmentListAPIView.as_view(), name='apartments')
+    path('apartments/', ApartmentListAPIView.as_view(), name='apartments')
 ]
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apartment',
     'corsheaders',
+    'apartment',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+AUTH_USER_MODEL = 'account.CustomUser'
