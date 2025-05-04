3b9dd4e715472dfda360a226f3614ed1d49b57c8
Muhammad <bakanaevmohmad@gmail.com>
2025-05-05 00:29:09 +0300

    Сделаны функции для валидации номера телефона

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
+import uuid
 
 
 # Create your models here.
-
+# Модели для аккаунта
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, phone_number, first_name, password=None, **extra_fields):
         if not email:
@@ -50,3 +52,19 @@ class CustomUser(AbstractUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+# Ниже будут модели для подтверждения номера телефона
+class PhoneConfirmation(models.Model):
+    token = models.UUIDField(default=uuid.uuid4, unique=True)
+    phone = models.CharField(max_length=20)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Данные для будушего CustomUser
+    email = models.EmailField()
+    password = models.CharField() #уже хешированный
+    first_name = models.CharField(max_length=150)
+
+    def is_expired(self):
+        return timezone.now() > self.created_at + timezone.timedelta(minutes=5)
