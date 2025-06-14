9880e4043c38904c7e6588972c80c4ce5d974d07
Senshi <bakanaevmohmad@gmail.com>
2025-06-14 18:21:21 +0300

    Настроил для сервера, но не проверено

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -e
 
-# Применение миграций
 python manage.py migrate --noinput
-
-# Сбор статики
 python manage.py collectstatic --noinput --clear
 
-# Запуск Gunicorn
-exec gunicorn --bind 0.0.0.0:8000 --workers 3 --access-logfile - config.wsgi:application
+exec gunicorn --bind 0.0.0.0:8000 --workers 3 config.wsgi:application
