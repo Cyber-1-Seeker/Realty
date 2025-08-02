87aceeb4658de36ab1ed07a35fd08678ec135e1b
Senshi <bakanaevmohmad@gmail.com>
2025-08-03 00:11:55 +0300

    Улучшил структуру папок для Home2

--- frontend/src/components/pages/Home2/AdvanceModal/AdvanceModal.jsx
+++ frontend/src/components/pages/Home2/AdvanceModal/AdvanceModal.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import {motion, AnimatePresence} from 'framer-motion';
 import styles from './AdvanceModal.module.css';
-import AdvancePaymentForm from './AdvancePaymentForm';
+import AdvancePaymentForm from './AdvancePaymentForm.jsx';
 
 const AdvanceModal = ({theme}) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
